d7fb9ec73a2902a9b0528e4bc2b6b89841d28e89
Nhlakanipho <nhlakaniphoradebe337@gmail.com>
2025-02-24 05:58:51 +0100

    Update: App.js navigation updated with a scanner screen

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import GuestRegisterScreen from './screens/GuestRegisterScreen';
 import GuestEmailScreen from './screens/GuestEmailScreen';
 import ForgetPasswordScreen from './screens/ForgetPasswordScreen';
 import PasswordEmailScreen from './screens/PasswordEmailScreen';
+import Scanner from './screens/Scanner';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="GetStartedScreen" component={GetStartedScreen} />
+        <Stack.Screen name="Scanner" component={Scanner} />
         <Stack.Screen name="ScanScreen" component={ScanScreen} />
         <Stack.Screen name="TraineeLoginScreen" component={TraineeLoginScreen} />
         <Stack.Screen name="GuestRegisterScreen" component={GuestRegisterScreen} />

98febced9ada5602d438c3193fbaffb9061915c7
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2025-02-24 11:33:24 +0200

    Update: navigation layout changed with proper naming

--- App.js
+++ App.js
@@ -4,13 +4,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './screens/SplashScreen';
 import GetStartedScreen from './screens/GetStartedScreen';
-import ScanScreen from './screens/ScanScreen';
 import TraineeLoginScreen from './screens/TraineeLoginScreen';
 import GuestRegisterScreen from './screens/GuestRegisterScreen';
 import GuestEmailScreen from './screens/GuestEmailScreen';
 import ForgetPasswordScreen from './screens/ForgetPasswordScreen';
 import PasswordEmailScreen from './screens/PasswordEmailScreen';
-import Scanner from './screens/Scanner';
+import PermissionsScreen from './screens/PermissionsScreen';
+import ScannerScreen from './screens/Scanner';
 
 const Stack = createStackNavigator();
 
@@ -21,8 +21,8 @@ export default function App() {
       <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
         <Stack.Screen name="GetStartedScreen" component={GetStartedScreen} />
-        <Stack.Screen name="Scanner" component={Scanner} />
-        <Stack.Screen name="ScanScreen" component={ScanScreen} />
+        <Stack.Screen name="ScannerScreen" component={ScannerScreen} />
+        <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
         <Stack.Screen name="TraineeLoginScreen" component={TraineeLoginScreen} />
         <Stack.Screen name="GuestRegisterScreen" component={GuestRegisterScreen} />
         <Stack.Screen name="GuestEmailScreen" component={GuestEmailScreen} />

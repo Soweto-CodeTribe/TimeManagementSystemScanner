a947ea9897378f5cac6d96a618e1ac9cdc073f7c
Nhlakanipho <nhalakaniphoradebe337@gmail.com>
2025-03-14 09:57:16 +0200

    Update: updated auth slice to include 12 hour format

--- Components/Redux/Slices/AuthenticationSlice.js
+++ Components/Redux/Slices/AuthenticationSlice.js
@@ -10,7 +10,7 @@ const getCurrentTime = () => {
   const ampm = hours >= 12 ? 'PM' : 'AM';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
-  return `${hours}:${minutes}`;
+  return `${hours}:${minutes} ${ampm}`;
   // ${ampm}
 };
 
